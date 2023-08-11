ff2b7ed42d31461bd1322e5c9c5fc550c2f23d31
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-08-11 13:16:05 +0300

    Create CalendarModeImpl.java

---
+++ Resources/Lesson04_2/refactor/CalendarModeImpl.java
@@ -0,0 +1,11 @@
+public class CalendarModeImpl implements Mode{
+    @Override
+    public void methodA() {
+        System.out.println("Action A in CalendarMode");
+    }
+
+    @Override
+    public void methodB() {
+        System.out.println("Action B in CalendarMode");
+    }
+}
