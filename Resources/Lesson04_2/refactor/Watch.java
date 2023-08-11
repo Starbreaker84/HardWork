073ba08ef7a2945c5965ca7f53e18475f7ce77e4
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-08-11 13:14:26 +0300

    Update Watch.java

--- Resources/Lesson04_2/refactor/Watch.java
+++ Resources/Lesson04_2/refactor/Watch.java
@@ -1,2 +1,21 @@
-public class Watch{
+public class Watch {
+    ModeCollection modes;
+    Mode currentMode;
+
+    Watch(ModeCollection modes) {
+        this.modes = modes;
+        this.currentMode = modes.getMode();
+    }
+
+    public void switchMode() {
+        currentMode = modes.getMode();
+    }
+
+    public void pushButtonA(Mode mode) {
+        mode.methodA();
+    }
+
+    public void pushButtonB(Mode mode) {
+        mode.methodB();
+    }
 }
