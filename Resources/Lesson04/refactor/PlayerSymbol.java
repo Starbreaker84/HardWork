2b8441b45d613710fb2c1e435c9ec4c066c8d8ba
Ruslan Omalaev <89082306+Starbreaker84@users.noreply.github.com>
2023-08-11 11:16:20 +0300

    Create PlayerSymbol.java

---
+++ Resources/Lesson04/refactor/PlayerSymbol.java
@@ -0,0 +1,15 @@
+public enum PlayerSymbol {
+    X("X"),
+    O("O"),
+    BLANK(" ");
+
+    private final String value;
+
+    PlayerSymbol(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
